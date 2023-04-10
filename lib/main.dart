40bf8a117b2b5f70b67e385799f0e86ccf0f18ce
Domengo <dominicsengo@gmail.com>
2023-04-10 03:05:06 +0300

    First Flutter

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: const Text('I Am Spurs'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/spurs.png'),
+        ),
+      ),
+    ),
+  ));
+}
