f1c1e58a4ae5fdd6d0088432c5831f1a4cd36638
Domengo <dominicsengo@gmail.com>
2023-04-10 19:52:54 +0300

    mi_card

--- mi_card_flutter-master/lib/main.dart
+++ mi_card_flutter-master/lib/main.dart
@@ -11,8 +11,87 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/dp.jpeg'),
+              ),
+              Text(
+                'Dominic Sengo',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'JetBrainsMono',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone_iphone_outlined,
+                        color: Colors.teal[900],
+                        size: 40,
+                      ),
+                      title: Text(
+                        '+254 702 060 203',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'JetBrainsMono',
+                          fontSize: 17.0,
+                        ),
+                      ),
+                    )),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email_sharp,
+                      color: Colors.teal[900],
+                      size: 40,
+                    ),
+                    title: Text(
+                      'dominicsengo@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'JetBrainsMono',
+                        fontSize: 12.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
